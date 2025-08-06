686fd4dedc9e07a431ca078bd797fd9cb0552a67
sherif <sherifdevv@gmail.com>
2025-08-06 15:29:03 +0300

    terms page added

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,4 @@
 // routes.js
-const routes = [
-  { path: "/", name: "لبيب" },
-  { path: "/terms", name: "الشروط والأحكام" },
-];
+const routes = [{ path: "/terms", name: "الشروط والأحكام" }];
 
 export default routes;
