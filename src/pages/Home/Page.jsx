bb039af540b15dd22103e32ecfb4f537eb27130e
sherif <sherifdevv@gmail.com>
2025-08-06 12:00:40 +0300

    home page updated

--- src/pages/Home/Page.jsx
+++ src/pages/Home/Page.jsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import Fields from "./components/Fields";
 import Initiatives from "./components/Initiatives";
 import Form from "./components/Form";
-import Register from "./components/register";
+import Register from "./components/Register";
 import Footer from "./components/Footer";
 
 export default function Page() {
