b7fc7eb86404b3b460dc7db59139d2b47637910c
sherif <sherifdevv@gmail.com>
2025-12-02 18:26:22 +0200

    fix: update Twitter link in Footer component to correct URL

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
     },
     {
       icon: "/twitter.svg",
-      link: "https://x.com/labeeb",
+      link: "https://x.com/mylabeeb?s=21",
     },
     {
       icon: "/whatsapp.svg",
